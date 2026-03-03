46090c4758901fbbfae0e2fe25fa8ce10e98ccc4
sibtain ali raza <sibtainaliraza369@gmail.com>
2026-03-03 19:23:39 +0530

    updated

--- Client/src/api/axiosInstance.ts
+++ Client/src/api/axiosInstance.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
 });
 
 API.interceptors.request.use((config) => {
     const token = localStorage.getItem("adminToken");
     if(token)
     {
-        config.headers.Authorization = `bearer ${token}`;
+        config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
 });
