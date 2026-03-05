6f086ec4c2f6b2b4474f4b5bbff2d639aec1a593
sibtain ali raza <sibtainaliraza369@gmail.com>
2026-03-06 01:28:16 +0530

    feat(utils): implement stream-based Cloudinary upload with filename preservation

--- server/src/utils/uploadToCloudinary.ts
+++ server/src/utils/uploadToCloudinary.ts
@@ -2,19 +2,25 @@ import cloudinary from "../config/cloudinary.js";
 import { UploadApiResponse } from "cloudinary";
 import streamifier from "streamifier";
 
-export const uploadCloudinary = (fileBuffer: Buffer, folder: string, isDocument:boolean = false): Promise<UploadApiResponse> =>{
-    return new Promise((resolve, reject)=>{
-        const uploadStream = cloudinary.uploader.upload_stream(
-            {
-                folder:`portfolio/${folder}`,
-
-                resource_type: isDocument ? "raw": "auto",
-            },
-            (error, result) => {
-                if(error) return reject(error);
-                if(result) resolve(result);
-            }
+export const uploadCloudinary = (
+  fileBuffer: Buffer, 
+  folder: string, 
+  isDocument: boolean = false
+): Promise<UploadApiResponse> => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        folder: `portfolio/${folder}`,
+        resource_type: isDocument ? "raw" : "auto",
+        use_filename: true,
+        unique_filename: false,
+        overwrite: true
+      },
+      (error, result) => {
+        if (error) return reject(error);
+        if (result) resolve(result);
+      }
     );
-        streamifier.createReadStream(fileBuffer).pipe(uploadStream);
-    });
+    streamifier.createReadStream(fileBuffer).pipe(uploadStream);
+  });
 };
