3990fbf5980b132d87da7d22ff09e0ddc6d905b5
sibtain ali raza <sibtainaliraza369@gmail.com>
2026-03-06 04:23:27 +0530

    added netlify link

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,7 +18,7 @@ app.use(helmet({
 }));
 app.use(cors({
     // Add your Netlify URL here
-    origin: ["http://localhost:5173", "https://sibtain-ali-raza.netlify.app/"], 
+    origin: ["http://localhost:5173", "https://sibtain-ali-raza.netlify.app"], 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
